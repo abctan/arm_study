75af59741d3c743db56d12198e9e308d185f10a2
twx5331771 <1948260215@qq.com>
2021-04-04 11:20:26 -0700

    添加使用SDK方式编写程序

---
+++ 04_led/main.c
@@ -0,0 +1,66 @@
+#include "fsl_common.h"
+#include "fsl_iomuxc.h"
+#include "MCIMX6Y2.h"
+
+// 时钟初始化
+void clock_init(void)
+{
+    CCM->CCGR1 = 0xFFFFFFFF;
+}
+
+// LED初始化
+void led_init(void)
+{
+    IOMUXC_SetPinMux(IOMUXC_GPIO1_IO03_GPIO1_IO03, 0);
+    IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO03_GPIO1_IO03, 0x10B0);
+    GPIO1->GDIR = 0x8;
+    GPIO1->DR = 0x0;
+}
+
+// 获取led状态 0:关  1:开
+volatile unsigned int get_led_status()
+{
+    volatile unsigned int status = (GPIO1->PSR >> 3) & 0x1;
+    return status == 0 ? 1 : 0;
+}
+
+// 修改LED灯状态
+void set_led_status(volatile unsigned int set_status)
+{
+    volatile unsigned int status;
+    volatile unsigned int tmp;
+
+    status = get_led_status();
+    if (status != set_status) {
+        tmp = GPIO1->GDIR & (~(0x1 << 3));
+        GPIO1->DR = tmp | ((set_status & 0x1) << 3);
+    }
+}
+
+// 延时函数
+void delay_short(volatile unsigned int n)
+{
+    while(n--) {}
+}
+void delay(volatile unsigned int n)
+{
+    while(n--) {
+        delay_short(0x7ff);
+    }
+}
+
+
+int main(void)
+{
+    clock_init();
+    led_init();
+
+    while (1) {
+        set_led_status(1);
+        delay(500);
+        set_led_status(0);
+        delay(500);
+    }
+
+    return 0;
+}
